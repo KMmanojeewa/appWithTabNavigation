7db9476e7d3011fd9caf4a6675879ccef01e492e
ss-koshala <koshala.manojeewa@silverstripers.com>
2021-03-19 23:11:51 +0530

    useNavigation usage

--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import {Button} from "react-native-web";
+import { useNavigation } from '@react-navigation/native';
 
 export default function NotificationsScreen() {
+
+
+    const navigation = useNavigation();
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Notifications!</Text>
+            <Button
+                title="Back"
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            />
         </View>
     );
 }
